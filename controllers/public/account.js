2cdc564b6344097d3f945ab55b6c3fe8e885d1a6
H3R0_2K17 <71520947+hero3sv@users.noreply.github.com>
2022-06-12 20:17:39 -0300

    mostrar los pedidos del cliente
    se muestran lo pedidos que ha realizado un cliente

--- controllers/public/account.js
+++ controllers/public/account.js
@@ -43,13 +43,12 @@ document.addEventListener('DOMContentLoaded', function () {
                                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                             <li><a class="dropdown-item" href="index.html">menu</a></li>
                                             <li><a class="dropdown-item" href="carts.html">anticonceptivo</a></li>
-                                            <li>
                                         </ul>
                                         <li class="nav-item">
                                         <a class="nav-link" href="carrito.html">Carrito</a>
                                          </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">revicion de receta</a>
+                                        <a class="nav-link" href="historial_pedidos.html">Historial de pedidos</a>
                                     </li>
                                     <li class="nav-item">
                                     <a class="nav-link" href="Contactanos.html">Contactanos</a>
