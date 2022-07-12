d07965cc0009e41f8d99e8de14066cc6a47ba625
2Miguel4 <a.alexrivas24@gmail.com>
2022-07-12 14:03:10 -0600

    registro privado
    registro de usuario privado

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -17,9 +17,9 @@ class Database
         // Credenciales para establecer la conexión con la base de datos.
        //Esas son las credenciales del ordenador personal de Miguel
         $server = 'localhost';
-        $database = 'medicfarmedition';
+        $database = 'medicfarm';
         $username = 'postgres';
-        $password = '1234';
+        $password = 'admin';
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:host=' . $server . ';dbname=' . $database . ';port=5432', $username, $password);
     }
