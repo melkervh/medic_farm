f69b6cb18282581627d98fbda20030792dc52ea1
2Miguel4 <a.alexrivas24@gmail.com>
2022-08-08 14:04:06 -0600

    login por fin
    se termina el login

--- controllers/dashboard/login.js
+++ controllers/dashboard/login.js
@@ -1,5 +1,5 @@
 // Constante para establecer la ruta y parámetros de comunicación con la API.
-const API_USUARIOS = SERVER + 'Actions/usuario.php?action=';
+const API_USUARIOS = SERVER + 'dashboard/usuario.php?action=';
 
 // Método manejador de eventos que se ejecuta cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', function () {
